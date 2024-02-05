9672d3ff13d054b1473842863969f5425139036a
Andrey Agarkov <agarkovand@gmail.com>
2024-02-05 09:38:59 +0100

    Unit Test for parse(date) added (Invalid date case)

--- src/test/java/com/dmdev/util/LocalDateFormatterTest.java
+++ src/test/java/com/dmdev/util/LocalDateFormatterTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LocalDateFormatterTest {
 
@@ -13,6 +14,12 @@ class LocalDateFormatterTest {
         LocalDate actual = LocalDateFormatter.parse("2009-03-25");
         LocalDate expected = LocalDate.of(2009, 3, 25);
         assertThat(expected).isEqualTo(actual);
+    }
+
+    @Test
+    void whenInValidDate_thenExceptionThrown() {
+        assertThatThrownBy(() -> LocalDateFormatter.parse("209-03-25"));
+        assertThatThrownBy(() -> LocalDateFormatter.parse(""));
     }
 
     @Test
